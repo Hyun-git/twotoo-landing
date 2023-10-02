bdcb26300455d2dbf3ef645c5b07aef7b2053c0a
Hyun_Gwang <mabr2845@gmail.com>
2023-10-02 16:40:55 +0900

    feature: landing-main

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Main from './component/Main/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Main/main.jsx
@@ -0,0 +1,27 @@
+import "./main.css";
+
+function Main() {
+  return (
+    <div className="Main">
+      <div className="Main-contents">
+        <img className="Main-twotoo" src="img/twotoo.png" />
+        <span className="Main-desc">너와 내가 함께 성장하는 과정</span>
+        <div className="Main-buttons">
+          <button className="Main-googleBtn">
+            <img src="img/button_google.png"></img>
+          </button>
+          <button className="Main-appleBtn">
+            <img src="img/button_apple.png" />
+          </button>
+        </div>
+        <img className="Main-flowers" src="img/flower.png"></img>
+        <img className="Main-bee" src="img/bee.png"></img>
+      </div>
+      <div className="Main-ground">
+        <img alt="Ground" src="img/ground.png" />
+      </div>
+    </div>
+  );
+}
+
+export default Main;

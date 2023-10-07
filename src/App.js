39899c3091c04f76eaccd2c6b26d61743fd65a69
Hyun_Gwang <mabr2845@gmail.com>
2023-10-07 15:58:55 +0900

    feat: add experience

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './component/Main/main';
+import Experience from './component/Experience/experience';
 
 function App() {
   return (
     <div className="App">
       <Main></Main>
+      <Experience></Experience>
     </div>
   );
 }
---
+++ src/component/Experience/experience.jsx
@@ -0,0 +1,20 @@
+import "./experience.css";
+
+function Experience() {
+  return (
+    <div className="Experience">
+      <div className="Experience-contents">
+        <img className="Experience-flower" src="img/Experience_Flower.png" />
+        <span className="Experience-title">
+          혼자가 아닌 함께 목표를 달성한 경험이 있나요?
+        </span>
+        <span className="Experience-desc">
+          상대방과 챌린지를 함께해 목표를 더 쉽게 달성할 수 있어요 <br />
+          매일 성장하는 과정을 기록해 보세요
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
